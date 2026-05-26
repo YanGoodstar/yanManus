826f4a0a4856f454bb18eb72a3e0590fe602c156
yan <yan@qq.com>
2026-05-26 14:18:34 +0800

    项目搭建

---
+++ src/main/java/com/zixiang/yanmanus/YanManusApplication.java
@@ -0,0 +1,13 @@
+package com.zixiang.yanmanus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanManusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YanManusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zixiang/yanmanus/YanManusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zixiang.yanmanus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanManusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
